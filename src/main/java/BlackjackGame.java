eebdbc5728e9562001d6db97e435bab763b9c7b6
JLian21UIC <jlian21@uic.edu>
2024-03-07 15:04:16 -0600

    implemented BlackjackGame class

---
+++ src/main/java/BlackjackGame.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class BlackjackGame {
+
+    ArrayList<Card> playerHand;
+    ArrayList<Card> bankerHand;
+
+    BlackjackDealer theDealer;
+    BlackjackGameLogic gameLogic;
+
+    double currentBet;
+    double totalWinnings;
+
+    public BlackjackGame(){
+        theDealer = new BlackjackDealer();
+        gameLogic = new BlackjackGameLogic();
+    }
+
+    public double evaluateWinnings(){
+        double winnings;
+
+        String winner = gameLogic.whoWon(playerHand, bankerHand);
+
+        if (winner.equals("push")){
+            winnings = 0.0;
+        }
+        else if (winner.equals("player")){
+
+            winnings = currentBet;
+
+            if (playerHand.size() == 2 && gameLogic.handTotal(playerHand) == 21){
+                winnings *= 1.5;
+            }
+
+        }
+        else{
+            winnings = -1 * currentBet;
+        }
+
+        totalWinnings += winnings;
+        return winnings;
+    }
+
+}
